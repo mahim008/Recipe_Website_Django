c61aa9e5a1df0c94f70cef64783285063c636966
Mahim <mahim.cse.uu@outlook.com>
2023-08-14 15:58:49 +0600

    completed registration arena

--- vege/views.py
+++ vege/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
 from .models import *
 
 
@@ -7,8 +9,30 @@ from .models import *
 def login_page(request):
     return render(request, 'login.html')
 
+
 def register_page(request):
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username=username)
+        if user.exists():
+            messages.info(request,'Username alreay taken! Try another username.')
+            return redirect('/register/')
+        user = User.objects.create(
+            first_name=first_name,
+            last_name=last_name,
+            username=username,
+        )
+        user.set_password(password)
+        user.save()
+        messages.info(request, 'Account successfully created')
+        return redirect('/register/')
+
     return render(request, 'register.html')
+
 
 def recipes(request):
     global context
@@ -32,7 +56,6 @@ def recipes(request):
 
     if request.GET.get('search'):
         queryset = queryset.filter(recipe_name__icontains=request.GET.get('search'))
-
 
     context = {'recipes': queryset}
 
